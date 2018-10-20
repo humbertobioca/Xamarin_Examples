fe0e72a45a18b710a183063c721ed2314c35a5a0
Humberto <humbertobioca@icloud.com>
2018-10-20 15:44:08 -0300

    Atualizando a lista de Exemplos
    Signed-off-by: Humberto <humbertobioca@icloud.com>

---
+++ ExemploBind/ExemploBind/MainPage.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExemploBind
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Label lblNovo = new Label();
+            lblNovo.TextColor = Color.Red;
+            lblNovo.HorizontalOptions = LayoutOptions.Center;
+
+            lblNovo.BindingContext = slider;
+            lblNovo.SetBinding(Label.TextProperty, "Value");
+
+            BoxView bx = new BoxView();
+            bx.Color = Color.Green;
+
+            bx.SetBinding(BoxView.OpacityProperty, new Binding
+            {
+                Source = slider,
+                Path = "Value"
+            });
+
+
+            stack.Children.Add(lblNovo);
+            stack.Children.Add(bx);
+        }
+    }
+}
---
+++ ExercicioDataBinding/ExercicioDataBinding/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace ExercicioDataBinding
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        void Handle_Completed(object sender, System.EventArgs e)
+        {
+            webView.Source = ((Entry)sender).Text; 
+        }
+
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            Button btn = (Button)sender;
+
+            switch (btn.StyleId)
+            {
+                case "avancar":
+                    webView.GoForward();
+                    break;
+                case "voltar":
+                    webView.GoBack();
+                    break;
+            }
+        }
+    }
+}
